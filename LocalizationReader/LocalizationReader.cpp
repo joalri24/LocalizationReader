47213402de062f3c3ef949a97faf493771bb225c
Jose Alejandro Riaño <joalri24@hotmail.com>
2017-12-09 16:27:01 -0500

    User can change input file

--- LocalizationReader/LocalizationReader.cpp
+++ LocalizationReader/LocalizationReader.cpp
@@ -7,6 +7,7 @@
 // Function declarations
 void printHeader();
 void printHelp();
+void selectFile(Reader &readerIn);
 
 // constants
 const string ENGLISH = "en";
@@ -15,6 +16,7 @@ const string HELP_COMMAND = "help";
 const string EXIT_COMMAND = "exit";
 const string CHANGE_TO_ES_COMMAND = "change to es";
 const string CHANGE_TO_EN_COMMAND = "change to en";
+const string CHANGE_FILE_COMMAND = "change file";
 
 int main()
 {	
@@ -24,16 +26,7 @@ int main()
 	// promt for the localization file.
 	string input;
 	cout << "Type the location of the xml file:\n";
-	bool validFile = false;
-	do
-	{
-		getline(cin, input);
-		if (!reader.setCurrentDocument(input))
-			cout << "Invalid file. Try again.\n";
-		else
-			validFile = true;
-	} while (validFile != true);
-	cout << "File opened succesfully!\n";
+	selectFile(reader);
 
 	// print some instructions
 	cout << "Type '" << HELP_COMMAND << "' for help.\n";
@@ -65,6 +58,14 @@ int main()
 			reader.setLanguage(SPANISH);
 			continue;
 		}
+
+		if (input == CHANGE_FILE_COMMAND)
+		{
+			cout << "Type the location of the xml file:\n";
+			selectFile(reader);
+			continue;
+		}
+
 		cout << reader.getString(input);
 		
 			
@@ -86,7 +87,23 @@ void printHelp()
 	cout << "*** help ***\n";
 	cout << "Type 'change to <language>' to change language. The possible values are:\n 'es' for spanish\n 'en' for english\n";
 	cout << "Example: change to en\n\n";
+	cout << "Type '"<< CHANGE_FILE_COMMAND << "' to change the input xml file.\n\n";
 	cout << "Type '"<< EXIT_COMMAND <<"' to quit.\n\n";
 	cout << "Type '"<< HELP_COMMAND  << "' for help.\n";
+}
+
+void selectFile(Reader &readerIn)
+{
+	bool validFile = false;
+	string input;
+	do
+	{		
+		getline(cin, input);
+		if (!readerIn.setCurrentDocument(input))
+			cout << "Invalid file. Try again.\n";
+		else
+			validFile = true;
+	} while (validFile != true);
+	cout << "File opened succesfully!\n";
 }
 
